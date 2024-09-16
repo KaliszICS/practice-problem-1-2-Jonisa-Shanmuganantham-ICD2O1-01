8217894ca94bd096260d786c2258b85010d7ee32
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-09-16 14:14:41 +0000

    first commit

--- PP1_2.py
+++ PP1_2.py
@@ -1,20 +1,46 @@
-
+'''
+    Lesson: Variables, Operators, Concatentation
+    Author: Jonisa Shanmuganantham
+    Date Created: Sept 16, 2024
+    Date Last Modified: Sept 16, 2024
+'''
 
 #Write Assignment code here
 
 def q1():
+  name = Alex
+  print(name)  
 
 def q2():
+    num1 = 2
+    num2 = 3
+    num3 = 5
+    print(num1 + num2 + num3)
+
 
 def q3():
+   num4 = 21
+   num5 = 4
+   print(num4 // num5)
 
 def q4():
+    num6 = 6.3
+    num7 = 3.0
+    print(num6 / num7)
 
 def q5():
+    word = Hello World
+    print(word)
+    word = Goodbye World
+    print(word)
 
 def q6():
+    tOrF = True
 
 def q7():
+    num8 = 7
+    num9 = 2
+    print(num8 % num9)
 
 q1()
 q2()
